e0340f0be27ae81592a192fa128f87c50bd354d8
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-03 08:46:53 -0800

    commit tree 1c9d92407ac96ab6f8a8ae2b5e380c5789638871

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`
+}
+
+const mondayWork = function (monday = 'go to the office') {
+    return `This Monday, I will ${monday}.`
+}
+
+const wrapAdjective = function (wrap = '*') {
+    const part1 = "You are";
+    return function (name = 'special') {
+        return `${part1} ${wrap}${name}${wrap}!`;
+    };
+}
